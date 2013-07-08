13c13f7893ce26b0815ad5b1c7ee7d2d55d72980
dimkas <dshekin@gmail.com>
2013-07-08 08:59:49 +0300

    add inicial support for serial usb

--- src/ua/kh/rit/MainActivity.java
+++ src/ua/kh/rit/MainActivity.java
@@ -1,27 +1,152 @@
 package ua.kh.rit;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
-import android.os.Bundle;
+import android.os.*;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.Button;
+import android.view.ViewGroup;
+import android.widget.*;
 
+import android.hardware.usb.UsbDevice;
+import android.hardware.usb.UsbManager;
+
+import com.hoho.android.usbserial.driver.UsbSerialDriver;
+import com.hoho.android.usbserial.driver.UsbSerialPort;
+import com.hoho.android.usbserial.driver.UsbSerialProber;
+import com.hoho.android.usbserial.util.HexDump;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends Activity implements View.OnClickListener {
 
+    private final String TAG = MainActivity.class.getSimpleName();
+
+
     Button butConf;
+    Button openSerial;
+
+    private UsbManager mUsbManager;
+    private ListView mListView;
+    private TextView mProgressBarTitle;
+    private ProgressBar mProgressBar;
+
+    private static final int MESSAGE_REFRESH = 101;
+    private static final long REFRESH_TIMEOUT_MILLIS = 5000;
+
+    private final Handler mHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case MESSAGE_REFRESH:
+                    refreshDeviceList();
+                    mHandler.sendEmptyMessageDelayed(MESSAGE_REFRESH, REFRESH_TIMEOUT_MILLIS);
+                    break;
+                default:
+                    super.handleMessage(msg);
+                    break;
+            }
+        }
+
+    };
+
+    /** Simple container for a UsbDevice and its driver. */
+    private static class DeviceEntry {
+        public UsbDevice device;
+        public UsbSerialDriver driver;
+        public int portIdx;
+
+        DeviceEntry(UsbDevice device, UsbSerialDriver driver, int portIdx) {
+            this.device = device;
+            this.driver = driver;
+            this.portIdx = portIdx;
+        }
+    }
+
+    private List<DeviceEntry> mEntries = new ArrayList<DeviceEntry>();
+    private ArrayAdapter<DeviceEntry> mAdapter;
+
     /**
      * Called when the activity is first created.
      */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        System.out.println(MainActivity.class.getSimpleName());
+
         setContentView(R.layout.main);
 
         butConf = (Button) findViewById(R.id.butConfActiviry);
         butConf.setOnClickListener(this);
 
+        //usb setup
 
+        mUsbManager = (UsbManager)getSystemService(Context.USB_SERVICE);
+        mListView = (ListView) findViewById(R.id.deviceList);
+        mProgressBar = (ProgressBar) findViewById(R.id.progressBar);
+        mProgressBarTitle = (TextView) findViewById(R.id.progressBarTitle);
+
+        mAdapter = new ArrayAdapter<DeviceEntry>(this, android.R.layout.simple_expandable_list_item_2, mEntries) {
+            @Override
+            public View getView(int position, View convertView, ViewGroup parent) {
+                final TwoLineListItem row;
+                if (convertView == null){
+                    final LayoutInflater inflater =
+                            (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                    row = (TwoLineListItem) inflater.inflate(android.R.layout.simple_list_item_2, null);
+                } else {
+                    row = (TwoLineListItem) convertView;
+                }
+
+                final DeviceEntry entry = mEntries.get(position);
+                final String title = String.format("Vendor %s Product %s",
+                        HexDump.toHexString((short) entry.device.getVendorId()),
+                        HexDump.toHexString((short) entry.device.getProductId()));
+                row.getText1().setText(title);
+
+                final String subtitle = entry.device != null ?
+                        entry.driver.getShortDeviceName() + " port " + entry.portIdx : "No Port";
+                row.getText2().setText(subtitle);
+
+                return row;
+            }
+
+        };
+        mListView.setAdapter(mAdapter);
+
+        mListView.setOnItemClickListener(new ListView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Log.d(TAG, "Pressed item " + position);
+                if (position >= mEntries.size()) {
+                    Log.w(TAG, "Illegal position.");
+                    return;
+                }
+
+                final DeviceEntry entry = mEntries.get(position);
+                final UsbSerialPort port = entry.driver.getPort(entry.portIdx);
+                if (port == null) {
+                    Log.d(TAG, "No port.");
+                    return;
+                }
+/*
+                try {
+                    Log.d(TAG, "set parameters");
+                    port.setParameters(9600, UsbSerialPort.DATABITS_8, UsbSerialPort.STOPBITS_1, UsbSerialPort.PARITY_NONE);
+                } catch (IOException ie) {
+                    Log.d(TAG, "exeption");
+                    ie.printStackTrace();
+                }
+*/
+
+                Log.d(TAG, "show console activity");
+                showConsoleActivity(port);
+            }
+        });
     }
 
     @Override
@@ -31,8 +156,76 @@ public class MainActivity extends Activity implements View.OnClickListener {
                    Intent intent = new Intent(this, ConfiguratorActivity.class);
                    startActivity(intent);
                    break;
+
                default:
                    break;
            }
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mHandler.sendEmptyMessage(MESSAGE_REFRESH);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mHandler.removeMessages(MESSAGE_REFRESH);
+    }
+
+    private void refreshDeviceList() {
+        showProgressBar();
+
+        new AsyncTask<Void, Void, List<DeviceEntry>>() {
+            @Override
+            protected List<DeviceEntry> doInBackground(Void... params) {
+                Log.d(TAG, "Refreshing device list ...");
+                SystemClock.sleep(1000);
+                final List<DeviceEntry> result = new ArrayList<DeviceEntry>();
+                for (final UsbDevice device : mUsbManager.getDeviceList().values()) {
+                    final UsbSerialDriver driver =
+                            UsbSerialProber.probeSingleDevice(device);
+                    Log.d(TAG, "Found usb device: " + device);
+                    if (driver == null) {
+                        Log.d(TAG, "  - No UsbSerialDriver available.");
+                        result.add(new DeviceEntry(device, null, 0));
+                    } else {
+                        Log.d(TAG, "  + " + driver + ", " + driver.getPortCount() + " ports.");
+                        for (int i = 0; i < driver.getPortCount(); ++i) {
+                            result.add(new DeviceEntry(device, driver, i));
+                        }
+                    }
+                }
+                return result;
+            }
+
+            @Override
+            protected void onPostExecute(List<DeviceEntry> result) {
+                mEntries.clear();
+                mEntries.addAll(result);
+                mAdapter.notifyDataSetChanged();
+                mProgressBarTitle.setText(
+                        String.format("%s device(s) found",Integer.valueOf(mEntries.size())));
+                hideProgressBar();
+                Log.d(TAG, "Done refreshing, " + mEntries.size() + " entries found.");
+            }
+
+        }.execute((Void) null);
+    }
+
+    private void showProgressBar() {
+        mProgressBar.setVisibility(View.VISIBLE);
+        mProgressBarTitle.setText(R.string.refreshing);
+    }
+
+    private void hideProgressBar() {
+        mProgressBar.setVisibility(View.INVISIBLE);
+    }
+
+    private void showConsoleActivity(UsbSerialPort port) {
+        Log.d(TAG, SerialConsoleActivity.class.getSimpleName() + "showConsoleActivity bla bla");
+        SerialConsoleActivity.show(this, port);
+    }
+
 }
---
+++ src/ua/kh/rit/SerialConsoleActivity.java
@@ -0,0 +1,174 @@
+
+
+package ua.kh.rit;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.hardware.usb.UsbManager;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ScrollView;
+import android.widget.TextView;
+
+import com.hoho.android.usbserial.driver.UsbSerialPort;
+import com.hoho.android.usbserial.util.HexDump;
+import com.hoho.android.usbserial.util.SerialInputOutputManager;
+
+import java.io.IOException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * Created by dimkas on 7/6/13.
+ */
+/**
+ * Monitors a single {@link com.hoho.android.usbserial.driver.UsbSerialDriver} instance, showing all data
+ * received.
+ *
+ * @author mike wakerly (opensource@hoho.com)
+ */
+public class SerialConsoleActivity extends Activity {
+
+    private final String TAG = SerialConsoleActivity.class.getSimpleName();
+    private final static String TAG1 = SerialConsoleActivity.class.getSimpleName();
+    /**
+     * Driver instance, passed in statically via
+     * {@link #show(Context, com.hoho.android.usbserial.driver.UsbSerialPort)}.
+     *
+     * <p/>
+     * This is a devious hack; it'd be cleaner to re-create the driver using
+     * arguments passed in with the {@link #startActivity(Intent)} intent. We
+     * can get away with it because both activities will run in the same
+     * process, and this is a simple demo.
+     */
+    private static UsbSerialPort sPort = null;
+
+    private UsbManager mUsbManager;
+
+    private TextView mTitleTextView;
+    private TextView mDumpTextView;
+    private ScrollView mScrollView;
+
+    private final ExecutorService mExecutor = Executors.newSingleThreadExecutor();
+
+    private SerialInputOutputManager mSerialIoManager;
+
+    private final SerialInputOutputManager.Listener mListener =
+            new SerialInputOutputManager.Listener() {
+
+                @Override
+                public void onRunError(Exception e) {
+                    Log.d(TAG, "Runner stopped.");
+                }
+
+                @Override
+                public void onNewData(final byte[] data) {
+                    SerialConsoleActivity.this.runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            SerialConsoleActivity.this.updateReceivedData(data);
+                        }
+                    });
+                }
+            };
+
+    public void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+
+        Log.d(TAG, SerialConsoleActivity.class.getSimpleName() + " onCreate()");
+
+        mUsbManager = (UsbManager) getSystemService(Context.USB_SERVICE);
+
+        mTitleTextView = (TextView) findViewById(R.id.demoTitle);
+        mDumpTextView = (TextView) findViewById(R.id.consoleText);
+        mScrollView = (ScrollView) findViewById(R.id.demoScroller);
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        stopIoManager();
+        if (sPort != null) {
+            try {
+                sPort.close();
+            } catch (IOException e) {
+                // Ignore.
+            }
+            sPort = null;
+        }
+        finish();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "Resumed, sDriver=" + sPort);
+        if (sPort == null) {
+            mTitleTextView.setText("No serial device.");
+        } else {
+            try {
+                Log.d(TAG, "open port");
+                sPort.open(mUsbManager);
+                Log.d(TAG, "set parameters port");
+                sPort.setParameters(9600, 8, UsbSerialPort.STOPBITS_1, UsbSerialPort.PARITY_NONE);
+            } catch (IOException e) {
+                Log.e(TAG, "Error setting up device: " + e.getMessage(), e);
+                mTitleTextView.setText("Error opening device: " + e.getMessage());
+                try {
+                    sPort.close();
+                } catch (IOException e2) {
+                    // Ignore.
+                }
+                sPort = null;
+                return;
+            }
+            mTitleTextView.setText("Serial device: " + sPort.getClass().getSimpleName());
+        }
+        onDeviceStateChange();
+    }
+
+    /**
+     * Starts the activity, using the supplied driver instance.
+     *
+     * @param context
+     * @param port
+     */
+    static void show(Context context, UsbSerialPort port) {
+        Log.d(TAG1, "seraioal consol activity show()");
+        sPort = port;
+        final Intent intent = new Intent(context, SerialConsoleActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP | Intent.FLAG_ACTIVITY_NO_HISTORY);
+        context.startActivity(intent);
+    }
+
+    private void stopIoManager() {
+        if (mSerialIoManager != null) {
+            Log.i(TAG, "Stopping io manager ..");
+            mSerialIoManager.stop();
+            mSerialIoManager = null;
+        }
+    }
+
+    private void startIoManager() {
+        if (sPort != null) {
+            Log.i(TAG, "Starting io manager ..");
+            mSerialIoManager = new SerialInputOutputManager(sPort, mListener);
+            mExecutor.submit(mSerialIoManager);
+        }
+    }
+
+    private void onDeviceStateChange() {
+        stopIoManager();
+        startIoManager();
+    }
+
+    private void updateReceivedData(byte[] data) {
+        final String message = "Read " + data.length + " bytes: \n"
+                + HexDump.dumpHexString(data) + "\n\n";
+        mDumpTextView.append(message);
+        mScrollView.smoothScrollTo(0, mDumpTextView.getBottom());
+    }
+}
